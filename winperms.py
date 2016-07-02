6ce0bc11891b160c0308f9aaf54cf68bc8b5ae35
Anthony Scudese <anthony@robottalk.tv>
2016-07-01 21:39:01 -0400

    Added propogation of inheritance

--- winperms.py
+++ winperms.py
@@ -6,6 +6,8 @@ import timeit
 import win32api
 import win32security
 import ntsecuritycon as con
+from functools import partial
+import copy
 
 pp = pprint.PrettyPrinter(indent=4)
 '''
@@ -43,11 +45,8 @@ def get_account_sids(accounts, users = {}):
 def get_account(name, domain = ''):
     return win32security.LookupAccountName(domain, name)
 
-def get_cache(sec_obj, users = {}, acls = {}):
-    print "Running process_security"
-    #pp.pprint(sec_obj)
+def get_user_cache(sec_obj, users = {}, acls = {}):
     for key in sec_obj:
-        print key
         current_obj = sec_obj[key]
         effective_acl = {}
         accounts = []
@@ -58,11 +57,6 @@ def get_cache(sec_obj, users = {}, acls = {}):
         except KeyError:
             pp.pprint(current_obj['owner'])
             raise KeyError("'%s' is not a valid security object! Missing owner parameter." % key)
-        try:
-            if current_obj['type'] not in ['file', 'folder', 'all']:
-                raise Exception("Valid type values are file, folder, all. On '%s' security obj" % key)
-        except KeyError:
-            raise KeyError("'%s' is not a valid security object! Missing type parameter." % key)
 
         try:
             if len(current_obj['acl']) > 0:
@@ -81,9 +75,42 @@ def get_cache(sec_obj, users = {}, acls = {}):
         users.update(get_account_sids(accounts, users))
         
         if current_obj.has_key('children'):
-            print "Has Children"
-            pp.pprint(current_obj['children'])
-            get_cache(current_obj['children'], users, acls)
+            #pp.pprint(current_obj['children'])
+            get_user_cache(current_obj['children'], users, acls)
+    return (users, acls)
+    
+def get_acl_cache(sec_obj, users = {}, acls = {}, inherit = {}):
+    for key in sec_obj:
+        current_inherit = copy.deepcopy(inherit)
+        current_obj = sec_obj[key]
+        try:
+            if current_obj['type'] not in ['file', 'folder', 'all']:
+                raise Exception("Valid type values are file, folder, all. On '%s' security obj" % key)
+        except KeyError:
+            raise KeyError("'%s' is not a valid security object! Missing type parameter." % key)
+        print "Inherited: %s" % key
+        pp.pprint(current_inherit)
+        try:
+            #pp.pprint(current_obj['acl'])
+            for x in range(len(current_obj['acl'])):
+                ace = current_obj['acl'][x]
+                try:
+                    if ace['inherit'] == 'true':
+                        if not current_inherit.has_key('acl'):
+                            current_inherit['acl'] = []
+                        current_inherit['acl'] += [ace]
+                except KeyError:
+                    pass
+        except KeyError:
+            raise KeyError("'%s' is not a valid security object! Missing acl parameter." % key)
+        '''try:
+            
+        except KeyError:
+            pass'''
+        
+        if current_obj.has_key('children'):
+            #pp.pprint(current_obj['children'])
+            get_acl_cache(current_obj['children'], users, acls, current_inherit)
     return (users, acls)
 def winperm(root_dir, perm_path):
     perm_fo = open(perm_path, 'r')
@@ -93,8 +120,11 @@ def winperm(root_dir, perm_path):
     users = {}
     explicit_perms = {}
     print "Looping"
-    (sec_users, sec_exp_perms) = get_cache(perm_obj)
-    pp.pprint(sec_users)
+    user_cache_times = timeit.Timer(partial(get_user_cache, perm_obj)).repeat(3, 1000)
+    user_cache_time = min(user_cache_times) / 1000
+    print "User Cache Time: %s" % user_cache_time
+    get_acl_cache(perm_obj)
+    #pp.pprint(sec_users)
     print "End Loop"
 
 
