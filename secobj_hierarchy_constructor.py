749c3f4b28c5a1e65747b19255cc7f60fcb7a26b
Anthony Scudese <anthony@robottalk.tv>
2017-10-19 18:33:50 -0400

    Multiple target directory support
    You can pass as many target directories as you like to create a full schema.

--- secobj_hierarchy_constructor.py
+++ secobj_hierarchy_constructor.py
@@ -1,22 +1,46 @@
-import os, sys
+import os
+import sys
 import argparse
-import pprint
 import json
 import re
 from collections import OrderedDict
 import copy
-def get_sec_obj(name, children = None):
+
+def merge(a, b, path=None):
+    "merges b into a"
+    if path is None: path = []
+    for key in b:
+        if key in a:
+            if isinstance(a[key], dict) and isinstance(b[key], dict):
+                merge(a[key], b[key], path + [str(key)])
+            elif a[key] == b[key]:
+                pass # same leaf value
+            else:
+                raise Exception('Conflict at %s' % '.'.join(path + [str(key)]))
+        else:
+            a[key] = b[key]
+    return a
+
+def getDefaultSecObj():
+    settings = getSecObjSettings()
+    settings['__DEFAULT__'] = copy.deepcopy(settings)
+    return settings
+
+
+def getSecObjSettings():
     settings = OrderedDict()
     settings['type'] = 'all'
     settings['owner'] = {"account": {"name": "Domain Admins", "domain": "NYC"}}
     settings['acl'] = []
     settings['skip'] = 'true'
 
-    sec_obj = OrderedDict()
-    sec_obj[name] = settings
-    sec_obj['__DEFAULT__'] = copy.deepcopy(settings)
+    return settings
 
-    if children != None or len(children) > 1:
+
+def get_sec_obj(name, children=None):
+    sec_obj = {name: getDefaultSecObj()}
+
+    if len(children) >= 1:
         sec_obj[name]['children'] = children
 
     return sec_obj
@@ -25,7 +49,6 @@ def get_sec_obj(name, children = None):
 def build_sec_obj(relative_path):
     path_components = relative_path.split(os.sep)
     path_components.reverse()
-    pprint.pprint(path_components)
     sec_obj = OrderedDict()
 
     for component in path_components:
@@ -35,7 +58,8 @@ def build_sec_obj(relative_path):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Generate Security Object JSON Schemas from Existing Directory Structures')
+    parser = argparse.ArgumentParser(
+        description='Generate Security Object JSON Schemas from Existing Directory Structures')
     parser.add_argument(
         '-s',
         '--start',
@@ -46,8 +70,9 @@ if __name__ == '__main__':
     parser.add_argument(
         '-t',
         '--target',
+        nargs='+',
         help='Absolute paths to target directories that are relative to start directory(-s).',
-        dest='targetPath',
+        dest='targetPaths',
         required=True
     )
     parser.add_argument(
@@ -61,14 +86,20 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     start_path = os.path.abspath(args.startPath)
+    schema_dict = {'__DEFAULT__': getSecObjSettings()}
+    for targetPath in args.targetPaths:
+        try:
+            relative_path = os.path.relpath(
+                os.path.abspath(targetPath), start_path)
+        except ValueError:
+            raise ValueError(
+                'The target_dir must be a path that is relative to the start path!')
+        else:
+            if relative_path[0] == '.':
+                raise ValueError(
+                    'target_path must be a path to a child under start_path!')
+        merge(schema_dict, build_sec_obj(relative_path))
 
-    try:
-        relative_path = os.path.relpath(os.path.abspath(args.targetPath), start_path)
-    except ValueError:
-        raise ValueError('The target_dir must be a path that is relative to the start path!')
-    else:
-        if relative_path[0] == '.':
-            raise ValueError('target_path must be a path to a child under start_path!')
-    print relative_path
-
-    secSchema = build_sec_obj(relative_path)
+    json.dump(schema_dict, args.jsonOutput, indent=4, separators=(',', ': '))
+    print "Wrote Schema to %s" % os.path.abspath(args.jsonOutput.name)
+    args.jsonOutput.close()
